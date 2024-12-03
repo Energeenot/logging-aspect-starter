45d9ec61d1b059f45dca7397c95de87aad8d5776
Energeenot <abramov.pandos@yandex.ru>
2024-12-03 18:54:07 +0300

    Конфиг для класса, который  создаёт таблицу, если она отсутствует

--- src/main/java/ru/Energeenot/logging_aspect_starter/config/DatabaseSchemaConfig.java
+++ src/main/java/ru/Energeenot/logging_aspect_starter/config/DatabaseSchemaConfig.java
@@ -1,16 +1,16 @@
 package ru.Energeenot.logging_aspect_starter.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.Energeenot.logging_aspect_starter.util.DatabaseSchemaValidator;
 
 @Configuration
-@EnableJpaRepositories
 public class DatabaseSchemaConfig {
 
     @Bean
+    @ConditionalOnMissingBean
     public JdbcTemplate jdbcTemplate(){
         return new JdbcTemplate();
     }
