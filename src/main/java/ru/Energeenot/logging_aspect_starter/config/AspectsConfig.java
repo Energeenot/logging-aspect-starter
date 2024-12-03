87d1700ba8e0c0494f61bd9882f49838703617e5
Energeenot <abramov.pandos@yandex.ru>
2024-12-03 18:53:09 +0300

    Изменил способ создания бинов, теперь нет конфликтов

--- src/main/java/ru/Energeenot/logging_aspect_starter/config/AspectsConfig.java
+++ src/main/java/ru/Energeenot/logging_aspect_starter/config/AspectsConfig.java
@@ -1,32 +1,37 @@
 package ru.Energeenot.logging_aspect_starter.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import ru.Energeenot.logging_aspect_starter.aspects.DataSourceErrorAspect;
+import ru.Energeenot.logging_aspect_starter.aspects.MetricAspect;
 import ru.Energeenot.logging_aspect_starter.kafka.MetricProducer;
 import ru.Energeenot.logging_aspect_starter.repository.DataSourceErrorLogRepository;
 import ru.Energeenot.logging_aspect_starter.service.ErrorLogService;
 
+@ComponentScan(basePackages = {"ru.Energeenot.logging_aspect_starter.config",
+                                "ru.Energeenot.logging_aspect_starter.repository"})
 @Configuration
+@Slf4j
 public class AspectsConfig {
 
-    private final MetricProducer metricProducer;
-    private final DataSourceErrorLogRepository errorLogRepository;
-
-    @Autowired
-    public AspectsConfig(MetricProducer metricProducer, DataSourceErrorLogRepository errorLogRepository) {
-        this.metricProducer = metricProducer;
-        this.errorLogRepository = errorLogRepository;
-    }
-
     @Bean
-    public ErrorLogService errorLogService() {
+    public ErrorLogService errorLogService(DataSourceErrorLogRepository errorLogRepository) {
+        log.info("ErrorLogService has been initialized");
         return new ErrorLogService(errorLogRepository);
     }
 
     @Bean
-    public DataSourceErrorAspect dataSourceErrorAspect() {
-        return new DataSourceErrorAspect(errorLogService(), metricProducer);
+    public DataSourceErrorAspect dataSourceErrorAspect(ErrorLogService errorLogService, @Qualifier("metricProducer") MetricProducer metricProducer) {
+        log.info("DataSourceErrorAspect has been initialized");
+        return new DataSourceErrorAspect(errorLogService, metricProducer);
+    }
+
+    @Bean
+    public MetricAspect metricAspect(@Qualifier("metricProducer") MetricProducer metricProducer) {
+        log.info("MetricAspect has been initialized");
+        return new MetricAspect(metricProducer);
     }
 }
